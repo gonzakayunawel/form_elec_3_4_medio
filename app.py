202844fb4c217c76b4677ce7273bdc125240b0af
Gonzalo Cayunao <go.cayunao@gmail.com>
2024-11-13 21:39:05 -0300

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 # Importat librerías
 import streamlit as st
 from form_validate import validate_form
-from control_form import verify_email
+
+
+
+# Cargar variables de entorno
+ELECTIVO_1 = st.secrets["ELECTIVO_1"]
+ELECTIVO_2 = st.secrets["ELECTIVO_2"]
+ELECTIVO_3 = st.secrets["ELECTIVO_3"]
+CURSOS = st.secrets["CURSOS"]
+ELECTIVOS_FG = st.secrets["ELECTIVOS_FG"]
 
 # Título
 st.title("Formulario de Inscripción de Electivos - 2024")
@@ -9,68 +17,55 @@ st.text("Recuerda que debes seguir las reglas para inscribirte correctamente.")
 
 # Identificación del estudiante
 with st.form(key="form"):
+
+    # --- Sección de identificación del estudiante ---
     st.header("Identificación del estudiante")
+
     # Nombre
     name = st.text_input("Ingresa tu nombre completo", placeholder="Nombre Completo")
     st.caption("Ejemplo: Francisca Alejandra Pérez Ortiz")
+
     # RUN
     run = st.text_input("Ingresa tu RUN", placeholder="12345678-k")
     st.caption(
         "Debes ingresar tu run sin puntos, con guión y dígito verificador. Ej.: 11222333-X"
     )
+
     # Email
     email = st.text_input(
         "Ingresa tu correo institucional",
         placeholder="nombre.apellido@estudiantes.colegiotgs.cl",
     )
     st.caption("Ejemplo: francisca.perez@estudiantes.colegiotgs.cl")
+
     # Curso
-    listado_cursos = ["2º medio G", "2º medio B"]
+    listado_cursos = CURSOS
     curso = st.radio("Selecciona tu curso", listado_cursos, index=None)
 
+    # --- Sección de inscripción Formación Diferenciada ---
     st.divider()
 
     # Seleccionar los electivos de formación diferenciada
     st.header("Electivos de Formación Diferenciada")
 
     # Electivo 1
-
-    listado_electivos_1 = [
-        "Área A: Comprensión Histórica del Presente",
-        "Área B: Límites, Derivadas e Integrales",
-        "Área C: Interpretación Musical",
-    ]
-
     electivo_1 = st.radio(
         "Selecciona el Electivo 1",
-        listado_electivos_1,
+        ELECTIVO_1,
         index=None,
     )
 
     # Electivo 2
-
-    listado_electivos_2 = [
-        "Área A: Taller de literatura",
-        "Área B: Biología celular y molecular",
-        "Área B: Pensamiento computacional y programación",
-        "Área C: Artes visuales, audiovisuales y multimediales",
-    ]
     electivo_2 = st.radio(
         "Selecciona el Electivo 2",
-        listado_electivos_2,
+        ELECTIVO_2,
         index=None,
     )
 
     # Electivo 3
-
-    listado_electivos_3 = [
-        "Área A: Lectura y escritura especializada",
-        "Área B: Química",
-        "Área C: Ciencias del Ejercicio Físico y Deportivo",
-    ]
     electivo_3 = st.radio(
         "Selecciona el Electivo 3",
-        listado_electivos_3,
+        ELECTIVO_3,
         index=None,
     )
 
@@ -79,13 +74,9 @@ with st.form(key="form"):
     # Electivos de Formación General
     st.header("Electivos de Formación General")
 
-    listado_electivos_fg = [
-        "Historia, Geografía y Cs. Sociales",
-        "Artes Visuales",
-    ]
     electivo_fg = st.radio(
         "Selecciona el Electivo de Formación General",
-        listado_electivos_fg,
+        ELECTIVOS_FG,
         index=None,
     )
     submit_button = st.form_submit_button(label="Enviar")
--- form_validate.py
+++ form_validate.py
@@ -1,139 +1,257 @@
 import streamlit as st
-import re, sqlite3
-from control_form import verify_email, insert_user_record
+import re
+import requests
 
-# Definir la expresión regular para un RUN chileno
-run_regex = r"^\d{7,8}-[\dKk]$"
-
-# Compilar la expresión regular
-pattern_run = re.compile(run_regex)
+# API_URL = st.secrets["API_URL"]
+API_URL = st.secrets["LOCAL_API_URL"]
+CUPO_ELECTIVO = int(st.secrets["CUPOS"])
 
 
 # Definir una función para validar el RUN chileno
 def is_valid_run(run):
+    # Definir la expresión regular para un RUN chileno
+    run_regex = r"^\d{7,8}-[\dKk]$"
+
+    # Compilar la expresión regular
+    pattern_run = re.compile(run_regex)
+
     return bool(pattern_run.fullmatch(run))
 
-
 # Definir una función para validar que los electivos no sean los 3 de la misma área
-def validate_electivos(electivo_1: str, electivo_2: str, electivo_3: str):
-    if electivo_1[:6] == electivo_2[:6] == electivo_3[:6]:
+def validate_electivos(electivo_1: str, electivo_2: str, electivo_3: str, electivo_fg: str) -> bool:
+    """ Validar que los electivos no sean los 3 de la misma área
+
+    Args:
+        electivo_1 (str): Electivo 1
+        electivo_2 (str): Electivo 2
+        electivo_3 (str): Electivo 3
+        electivo_fg (str): Electivo de formación general
+
+    Returns:
+        bool: True si los electivos no son los 3 de la misma área, False en caso contrario
+    """
+    if electivo_1 is None or electivo_2 is None or electivo_3 is None or electivo_fg is None:
+        return False
+    elif electivo_1[:6] == electivo_2[:6] == electivo_3[:6]:
         return False
     else:
         return True
 
+# Verificar si el correo electrónico es válido para inscribirse
+def verify_email(email:str) -> bool:
+    """ Verificar si el correo electrónico es válido para inscribirse
 
-# Definir una función para validar si el electivo de formación difereniada tiene cupo
+    Args:
+        email (str): Correo electrónico
 
-
-def validate_elective_availability(
-    electivo_elegido: str, electivo: str, availavity: int
-):
-    conn = sqlite3.connect("form_app.db")
-    cursor = conn.cursor()
-
+    Returns:
+        bool: True si el correo electrónico es válido, False en caso contrario
+    """
     try:
-        cursor.execute(
-            f"SELECT COUNT({electivo}) FROM users_register WHERE {electivo} = ?",
-            (electivo_elegido,),
-        )
-        result = cursor.fetchone()
-        if result[0] >= availavity:
-            return False
-        else:
-            return True
-    except Exception as e:
-        raise e
-    finally:
-        conn.close()
+        response = requests.get(f"{API_URL}/verify_email/{email}")
+        response.raise_for_status()
+        data = response.json()
 
-    # Definir una función para validar si el electivo de formación general tiene cupo4
+        return True if data["message"] == True else False
 
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
 
-def validate_elective_availability_fg(
-    electivo_fg: str,
-    curso: str,
-    availavity: int,
-):
-    conn = sqlite3.connect("form_app.db")
-    cursor = conn.cursor()
+def verify_user(run: str, email: str, curso: str) -> bool:
+    """ Verificar si el RUN, email y curso corresponden al usuario
 
-    try:
-        cursor.execute(
-            f"SELECT COUNT(electivo_fg) FROM users_register WHERE electivo_fg = ? AND curso = ?",
-            (electivo_fg, curso),
-        )
-        result = cursor.fetchone()
-        if result[0] >= availavity:
-            return False
-        else:
-            return True
-    except Exception as e:
-        raise e
-    finally:
-        conn.close()
+    Args:
+        run (str): RUN
+        email (str): Correo electrónico
+        curso (str): Curso
 
-
-# Validar que el run se relacione al email
-
-
-def validate_run_email(run: str, email: str, table_name="users_register"):
-    conn = sqlite3.connect("form_app.db")
-    cursor = conn.cursor()
-
-    try:
-        cursor.execute(
-            f"SELECT run, email FROM {table_name} WHERE run = ? AND email = ?",
-            (run, email),
-        )
-        result = cursor.fetchone()
-        return True if result else False
-
-    except Exception as e:
-        raise e
-    finally:
-        conn.close()
-
-# Verificar si alguno de los electivos ya fue realizado en el año anterior
-
-def verify_electivo_prior_year(run: str, electivo: str):
-
+    Returns:
+        bool: True si corresponden, False en caso contrario
+    """
     run = run.replace("-", "")
-    electivo_cleaned = electivo[8:]
-
-    conn = sqlite3.connect("form_app.db")
-    cursor = conn.cursor()
-
     try:
-        cursor.execute(
-            f"SELECT run, electivo_1, electivo_2, electivo_3 FROM listado_electivos_2024_3_medio WHERE run = ?",
-            (run,),
-        )
-        result = cursor.fetchone()
-        if result:
-            if electivo_cleaned in list(result[1:]):
-                return True
-            else:
-                return False
-        else:
-            return False
-    except Exception as e:
-        raise e
-    finally:
-        conn.close()
+        response = requests.get(f"{API_URL}/verify_user/{run}/{email}/{curso}")
+        response.raise_for_status()
+        data = response.json()
 
+        return True if data["message"] == True else False
 
-# Cupos formación diferenciada
-cupos_electivos = 10
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
 
-# Cupos formación general
-cupos_electivos_fg = 10
+# Verificar si el email ya está registrado
+def check_student_record( email: str) -> bool:
+    """ Verificar si el email ya está registrado
 
+    Args:
 
+        email (str): Correo electrónico
+
+    Returns:
+        bool: True si está registrado, False caso contrario
+    """
+    try:
+        response = requests.get(f"{API_URL}/check_student_record/{email}")
+        response.raise_for_status()
+        data = response.json()
+
+        return True if data["message"] == True else False
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
+
+# Verificar que hay cupos disponibles para el electivo
+def validate_elective_availability(electivo_seleccionado: str, columna_electivo: str, cupos_electivos: int) -> bool:
+    """ Verificar que hay cupos disponibles para el electivo
+
+    Args:
+        electivo (str): Electivo
+        electivo_name (str): Nombre del electivo
+        cupos_electivos (int): Número de cupos disponibles para el electivo
+
+    Returns:
+        bool: True si hay cupos disponibles, False en caso contrario
+    """
+    try:
+        response = requests.get(f"{API_URL}/check_elective_class_availability/{electivo_seleccionado}/{columna_electivo}/{cupos_electivos}")
+        response.raise_for_status()
+        data = response.json()
+
+        return True if data["message"] == True else False
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
+
+# Verificar que hay cupos disponibles para el electivo de formación general
+def validate_elective_availability_fg(electivo_seleccionado: str, curso: str, cupos_electivos_fg: int) -> bool:
+    """ Verificar que hay cupos disponibles para el electivo de formación general
+
+    Args:
+        electivo (str): Electivo
+        electivo_name (str): Nombre del electivo
+        cupos_electivos (int): Número de cupos disponibles para el electivo
+
+    Returns:
+        bool: True si hay cupos disponibles, False en caso contrario
+    """
+    try:
+        response = requests.get(f"{API_URL}/check_elective_class_availability_fg/{electivo_seleccionado}/{curso}/{cupos_electivos_fg}")
+        response.raise_for_status()
+        data = response.json()
+
+        return True if data["message"] == True else False
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
+
+# Vetificar si el estudiante está inscrito en el electivo anterior
+def check_student_enrolled_in_previous_elective(run: str, electivo: str) -> bool:
+    """ Vetificar si el estudiante está inscrito en el electivo anterior
+
+    Args:
+        run (str): RUN
+        electivo (str): Electivo
+
+    Returns:
+        bool: True si está inscrito, False en caso contrario
+    """
+    try:
+        response = requests.get(f"{API_URL}/check_student_enrolled_in_previous_elective/{run}/{electivo}")
+        response.raise_for_status()
+        data = response.json()
+
+        return True if data["message"] == True else False
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
+
+# Insertar registro en la base de datos de inscripciones
+def insert_user_record(
+    name,
+    run,
+    email,
+    curso,
+    electivo_1,
+    electivo_2,
+    electivo_3,
+    electivo_fg,
+):
+    """ Insertar registro en la base de datos de inscripciones
+
+    Args:
+        name (str): Nombre
+        run (str): RUN
+        email (str): Correo electrónico
+        curso (str): Curso
+        electivo_1 (str): Electivo 1
+        electivo_2 (str): Electivo 2
+        electivo_3 (str): Electivo 3
+        electivo_fg (str): Electivo de formación general
+
+    Returns:
+        bool: True si se insertó correctamente, False en caso contrario
+    """
+    try:
+        response = requests.post(f"{API_URL}/insert_user_record", json={
+            "name": name,
+            "run": run,
+            "email": email,
+            "curso": curso,
+            "electivo_1": electivo_1,
+            "electivo_2": electivo_2,
+            "electivo_3": electivo_3,
+            "electivo_fg": electivo_fg,
+        })
+        response.raise_for_status()
+        data = response.json()
+
+        return True if data["message"] == True else False
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f'Error HTTP: {http_err}')
+    except requests.exceptions.RequestException as err:
+        print(f'Error en la solicitud: {err}')
+    except ValueError:
+        print('Error al procesar JSON: la respuesta no es un JSON válido')
+
+# Validar el formulario
 def validate_form(
-    name, run, email, curso, electivo_1, electivo_2, electivo_3, electivo_fg
+    name,
+    run,
+    email,
+    curso,
+    electivo_1,
+    electivo_2,
+    electivo_3,
+    electivo_fg
 ):
     if run and not is_valid_run(run):  # verif icar si el RUN es válido
         st.error("RUN no válido")
+
     elif email and not verify_email(
         email
     ):  # verificar si el email es válido y si existe en la base de datos
@@ -141,79 +259,58 @@ def validate_form(
             "El email ingresado no es válido o no es un email habilitado para inscribirse"
         )
     else:
-        # Diccionario que mapea las variables a sus respectivos mensajes de error
-        form_fields = {
-            "name": "Debes ingresar el nombre completo",
-            "run": "Debes ingresar el RUN",
-            "email": "Debes ingresar tu email",
-            "curso": "Debes seleccionar tu curso",
-            "electivo_1": "Debes seleccionar el electivo 1",
-            "electivo_2": "Debes seleccionar el electivo 2",
-            "electivo_3": "Debes seleccionar el electivo 3",
-            "electivo_fg": "Debes seleccionar el electivo de formación general",
-        }
-
-        # Verificar cada campo en el diccionario
-        for field, menssage in form_fields.items():
-            if not locals()[field]:  # `locals()` permite obtener el valor de la variable por nombre
-                st.error(menssage)
-                break  # Salir del bucle después de mostrar el primer error encontrado
-        if not electivo_fg:
+        if not verify_user(run, email, curso):
             st.error(
-                "Debes seleccionar el electivo de formación general"
-            )  # verificar si el electivo FG ha sido seleccionado
-        elif not validate_run_email(run, email, table_name="emails"):
-            st.error(
-                "El RUN o email no se corresponden con el usuario registrado. Verifca que tu RUN y correo electrónico sean correctos."
+                "El RUN, email o curso no se corresponden. Verifca que tu RUN, correo y curso sean correctos."
             )
 
         else:
-            if verify_email(email, table="users_register"):
+            if check_student_record(email):
                 st.error(
                     "El email ingresado ya fue registrado"
                 )  # verificar si el email ya está en la base de datos de inscripciones
 
-            elif not validate_electivos(electivo_1, electivo_2, electivo_3):
+            elif not validate_electivos(electivo_1, electivo_2, electivo_3, electivo_fg):
                 st.error(
-                    "Los electivos no pueden ser los 3 de la misma área"
+                    "Los electivos no pueden ser los 3 de la misma área o hay un electivo no seleccionado. Por favor, selecciona los electivos que quieres inscribirte."
                 )  # verificar si los electivos no son los 3 de la misma área
 
             elif not validate_elective_availability(
-                electivo_1, "electivo_1", cupos_electivos
+                electivo_1, "electivo_1", CUPO_ELECTIVO
             ):
                 st.error(
                     f"Error. Electivo: {electivo_1} sin cupo."
                 )  # verificar si el electivo posee disponibilidad
 
             elif not validate_elective_availability(
-                electivo_2, "electivo_2", cupos_electivos
+                electivo_2, "electivo_2", CUPO_ELECTIVO
             ):
                 st.error(
                     f"Error. Electivo: {electivo_2} sin cupo."
                 )  # verificar si el electivo posee disponibilidad
 
             elif not validate_elective_availability(
-                electivo_3, "electivo_3", cupos_electivos
+                electivo_3, "electivo_3", CUPO_ELECTIVO
             ):
                 st.error(
                     f"Error. Electivo: {electivo_3} sin cupo."
                 )  # verificar si el electivo posee disponibilidad
 
-            elif not validate_elective_availability_fg(
-                electivo_fg, curso, cupos_electivos_fg
+            elif validate_elective_availability_fg(
+                electivo_fg, curso, CUPO_ELECTIVO
             ):
                 st.error(
                     f"Error. Electivo: {electivo_fg} sin cupo para el curso: {curso}."
                 )
-            elif verify_electivo_prior_year(run, electivo_1):
+            elif check_student_enrolled_in_previous_elective(run, electivo_1):
                 st.error(
                     f"Error. El electivo {electivo_1} ya fue realizado en el año anterior."
                 )
-            elif verify_electivo_prior_year(run, electivo_2):
+            elif check_student_enrolled_in_previous_elective(run, electivo_2):
                 st.error(
                     f"Error. El electivo {electivo_2} ya fue realizado en el año anterior."
                 )
-            elif verify_electivo_prior_year(run, electivo_3):
+            elif check_student_enrolled_in_previous_elective(run, electivo_3):
                 st.error(
                     f"Error. El electivo {electivo_3} ya fue realizado en el año anterior."
                 )
